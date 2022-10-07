a3f248046dacdcc075513d659ef885e81c933deb
Eli Porcel <114632682+EliPorcel@users.noreply.github.com>
2022-10-07 19:33:06 -0300

    Add files via upload

---
+++ hola.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace presentacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.ForegroundColor=ConsoleColor.Magenta;
+            Console.WriteLine("Para iniciar, presione una tecla cualquiera...");
+            Console.ReadKey(); 
+            Console.ForegroundColor=ConsoleColor.DarkYellow;
+            Console.WriteLine("¿Cuál es cualquiera?");
+            Console.ReadKey();
+            Console.ForegroundColor=ConsoleColor.Green; 
+            Console.WriteLine("Veo la Esc,...");
+            Console.ReadKey();
+            Console.ForegroundColor=ConsoleColor.Cyan; 
+            Console.WriteLine("...Ctrl...");
+            Console.ReadKey();
+            Console.ForegroundColor=ConsoleColor.Yellow; 
+            Console.WriteLine("...y Pg Up"); 
+            Console.ReadKey();
+            Console.ForegroundColor=ConsoleColor.Red;
+            Console.WriteLine("¡No tiene tecla Cualquiera!"); 
+            Console.ReadKey();
+            Console.ForegroundColor=ConsoleColor.Blue;
+            Console.WriteLine("Tanta computación me dio sed, creo que aquí hay cerveza Tab.");
+            Console.ReadKey(); 
+            Console.BackgroundColor=ConsoleColor.Red;
+            Console.ForegroundColor=ConsoleColor.Black;
+            Console.WriteLine("¡Ahh no hay tiempo para eso, finalizó la computadora!");
+            Console.ReadKey();
+            Console.ForegroundColor=ConsoleColor.White;
+            Console.BackgroundColor=ConsoleColor.Black;
+            Console.WriteLine("¡Gracias! Presione una tecla cualquiera para salir.");
+            Console.ReadKey();
+        }
+    }
+}
+        
+    
+
